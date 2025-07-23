b8e69f01addee9ebc18e7acae998423169a7adf6
Simegneww <usesimegn@gmail.com>
2025-07-23 23:06:28 +0300

    EXPERIMENT: Literal path for checker

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self): return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.PositiveIntegerField()
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)

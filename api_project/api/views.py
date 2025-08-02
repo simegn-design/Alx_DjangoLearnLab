425c64e1f25e81678f17163a2bdedb4e5fd88328
Simegneww <usesimegn@gmail.com>
2025-08-02 16:00:24 +0300

    Completed all API tasks with authentication

---
+++ api_project/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

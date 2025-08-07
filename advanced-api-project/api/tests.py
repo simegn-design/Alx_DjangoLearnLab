4b1adbb6039354802830953ac93895385e844b3f
Simegneww <usesimegn@gmail.com>
2025-08-08 02:09:03 +0300

    Complete test coverage for all endpoints

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,47 +1,35 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
-from django.test import override_settings
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
 from .models import Author, Book
 
-@override_settings(
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:'
-        }
-    }
-)
 class BookAPITests(APITestCase):
     def setUp(self):
+        # Create test data
         self.author = Author.objects.create(name="Test Author")
         self.book = Book.objects.create(
             title="Sample Book",
             publication_year=2020,
             author=self.author
         )
+        
+        # Create test user
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        
+        # Create authenticated client
+        self.auth_client = APIClient()
+        self.auth_client.force_authenticate(user=self.user)
 
-    def test_book_list_status(self):
-        url = reverse('book-list')
+    # Unauthenticated tests
+    def test_book_list_unauthenticated(self):
+        url = reverse('api:book-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_book_search(self):
-        url = reverse('book-list') + '?search=Sample'
+    def test_book_detail_unauthenticated(self):
+        url = reverse('api:book-detail', kwargs={'pk': self.book.pk})
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
 
-    def test_book_ordering(self):
-        url = reverse('book-list') + '?ordering=title'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_create_book_unauthorized(self):
-        url = reverse('book-create')
-        data = {
-            "title": "New Book",
-            "publication_year": 2023,
-            "author": self.author.id
-        }
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, 403)  # Changed from 401 to 403
